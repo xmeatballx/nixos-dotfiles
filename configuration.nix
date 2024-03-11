8a0a5b331ed9354ef5bd942c8d8920ff4e70d5f7
xmeatballx <erik.jensen5@pcc.edu>
2024-03-10 23:43:56 -0400

    40 current  2024-03-10 17:24:30  23.11.20240305.880992d   6.1.80                          *

--- configuration.nix
+++ configuration.nix
@@ -1,35 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];    
   environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
 
-  # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -44,7 +29,6 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-   # Configure keymap in X11
   services.xserver = {
     enable = true;
 
@@ -53,8 +37,6 @@
     };
    
     displayManager = {
- #     autoLogin.enable = true;
-  #    autoLogin.user = "meatball";
       defaultSession = "none+i3";
     };
 
@@ -62,9 +44,9 @@
       enable = true;
       package = pkgs.i3-gaps;
       extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3lock #default i3 screen locker
+        dmenu
+        i3status
+        i3lock
         feh
         picom
      ];
@@ -76,7 +58,6 @@
   services.xrdp.defaultWindowManager = "${pkgs.i3-gaps}/bin/i3";
   services.xrdp.openFirewall = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.meatball = {
     isNormalUser = true;
     description = "meat ball";
@@ -84,15 +65,12 @@
     packages = with pkgs; [];
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   virtualisation.docker.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     vim 
      git
      kitty
      docker-compose
@@ -103,14 +81,6 @@
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   programs.zsh = {
     enable = true;
@@ -125,9 +95,7 @@
   };
 
   users.users.meatball.shell = pkgs.zsh;
-  # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.settings.PasswordAuthentication = true;
 
@@ -136,7 +104,6 @@
      enable = true;
      recommendedProxySettings = true;
      #recommendedTlsSettings = true;
-      # other Nginx options
      virtualHosts."meatball.dev.local" =  {
        #enableACME = true;
        #forceSSL = true;
@@ -157,12 +124,8 @@
     defaults.email = "erik.rjensen@yahoo.com";
   };
 
-
-  # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   networking.firewall.allowedUDPPorts = [ 80 443 ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- home.nix
+++ home.nix
@@ -10,8 +10,6 @@ let
   mod      ="Mod4";
 in 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "meatball";
   home.homeDirectory = "/home/meatball";
 
@@ -24,8 +22,6 @@ in
   # release notes.
   home.stateVersion = "23.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     image-roll
     nitrogen
@@ -75,14 +71,7 @@ in
     '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
     ".config/nvim" = {
       source = ./config/nvim;
       recursive = true;
@@ -95,25 +84,8 @@ in
     ".config/picom/default.conf" = {
         source = ./config/picom/default.conf;
     };
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/meatball/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "nvim";
   };
@@ -191,7 +163,6 @@ in
       startup = [ 
         { command = "--no-startup-id nitrogen --set-zoom-fill .config/wallpapers/wallpaper.png"; always = true; }
         { command = "picom --daemon --config ~/.config/picom/default.conf"; always = true; } 
-       # { command = "--no-startup-id picom --daemon"; always = true; } 
       ];
     };
   };
