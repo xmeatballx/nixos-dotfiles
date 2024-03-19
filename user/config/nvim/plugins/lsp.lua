398fbc2cbc19869778e9cf896f3f44ac3b6271ab
xmeatballx <erik.jensen5@pcc.edu>
2024-03-19 17:16:31 -0400

    19 current  2024-03-19 01:03:48  23.11.20240305.880992d   6.1.80                          *

--- user/config/nvim/plugins/lsp.lua
+++ user/config/nvim/plugins/lsp.lua
@@ -3,7 +3,7 @@ local lspconfig = require('lspconfig')
 lspconfig.lua_ls.setup {}
 lspconfig.rnix.setup {}
 lspconfig.tsserver.setup {}
-lspconfig.svelte_ls.setup {}
+lspconfig.svelteserver.setup {}
 
 
 -- Global mappings.
