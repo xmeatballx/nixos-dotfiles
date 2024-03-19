2c1401e6968e4b11a27fced34954edc2fb5c85ae
xmeatballx <erik.jensen5@pcc.edu>
2024-03-18 23:32:30 -0400

    18 current  2024-03-18 00:57:37  23.11.20240305.880992d   6.1.80                          *

--- user/colors/catppuccin-mocha.nix
+++ user/colors/catppuccin-mocha.nix
@@ -4,5 +4,6 @@
   lavender = "#b4befe";
   text = "#cdd6f4";
   overlay0 = "#6c7086";
+  surface1 = "#45475a";
   base = "#1e1e2e";
 }
--- user/i3/bar.nix
+++ user/i3/bar.nix
@@ -1,9 +1,6 @@
 { config, ... }:
-let
-  colors = import ../colors/catppuccin-mocha.nix;
-in
 {
-  xsession.windowManager.i3.config.bars = with colors; [
+  xsession.windowManager.i3.config.bars = [
     {
       position = "top";
       statusCommand = "i3status";
@@ -12,32 +9,6 @@ in
         size = 11.0;
       };
       trayOutput = "primary";
-      colors = {
-        background = "${base}";
-        statusline = "${text}";
-        focusedStatusline = "${text}";
-        focusedSeparator = "${base}";
-        focusedWorkspace = {
-          border = "${base}";
-          background = "${base}";
-          text = "${rosewater}";
-        };
-        inactiveWorkspace = {
-          border = "${base}";
-          background = "${base}";
-          text = "${overlay0}";
-        };
-        urgentWorkspace = {
-          border = "${base}";
-          background = "${base}";
-          text = "${overlay0}";
-        };
-        bindingMode = {
-          border = "${base}";
-          background = "${base}";
-          text = "${overlay0}";
-        };
-      };
     }
   ];
 }
