72680534c5cf7f2358a35ca766a28f6fc2cc1acc
xmeatballx <erik.jensen5@pcc.edu>
2024-03-21 16:09:03 -0400

    19 current  2024-03-19 01:03:48  23.11.20240305.880992d   6.1.80                          *

--- user/config/nvim/plugins/lsp.lua
+++ user/config/nvim/plugins/lsp.lua
@@ -1,6 +1,32 @@
 -- Setup language servers.
 local lspconfig = require('lspconfig')
-lspconfig.lua_ls.setup {}
+lspconfig.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 lspconfig.rnix.setup {}
 lspconfig.tsserver.setup {}
 lspconfig.svelte.setup {}
